41fa5287d640ee335c975370813d766fb08a2c28
Patrick Baselier <patrick@kabisa.nl>
2012-03-27 18:30:07 +0200

    Added capistrano (take 1).

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ gem 'rails', '3.2.2'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'mysql2'
+gem "capistrano", "~> 2.11.2"
+gem "rvm-capistrano", "~> 1.0.1"
 
 
 # Gems used only for assets and not required
---
+++ config/deploy.rb
@@ -0,0 +1,58 @@
+# RVM bootstrap
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require 'rvm/capistrano'
+
+# When using whenever gem:
+# set :whenever_command, "bundle exec whenever"
+# require 'whenever/capistrano'
+
+set :rvm_ruby_string, '1.9.2-p136@guaranteed'
+set :rvm_type, :user
+
+# bundler bootstrap
+require 'bundler/capistrano'
+
+# main details
+set :application, "guaranteed"
+role :web, "patrick.kabisa.nl"
+role :app, "patrick.kabisa.nl"
+role :db,  "patrick.kabisa.nl", :primary => true
+
+# server details
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+set :deploy_to, "/var/www/guaranteed"
+set :deploy_via, :remote_cache
+set :user, "passenger"
+set :use_sudo, false
+
+# repo details
+set :scm, :git
+set :scm_username, "passenger"
+set :repository, "git@github.com:bazzel/guaranteed.git"
+set :branch, "master"
+set :git_enable_submodules, 1
+
+# tasks
+namespace :deploy do
+  task :start, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  desc "Symlink shared resources on each release - not used"
+  task :symlink_shared, :roles => :app do
+    #run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+end
+
+after 'deploy:update_code', 'deploy:symlink_shared'
+
