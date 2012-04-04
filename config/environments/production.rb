b7ba1aa87d0fd7ac102a77fbd7dbd3aed3b533db
Patrick Baselier <patrick@kabisa.nl>
2012-04-04 14:57:49 +0200

    Solved 'hostname was not match with the server certificate'.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,6 +52,12 @@ Warranties::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'patrickbaselier.nl' }
 
+  # Turn off 'enable_starttls_auto' to avoid
+  # 'OpenSSL::SSL::SSLError (hostname was not match with the server certificate)'
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => false
+  }
+
   # Enable threaded mode
   # config.threadsafe!
 
